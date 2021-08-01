ed024d2a4b595f0894d889f58c5d7aa439abc53a
Ligalaiz <slavakuz88@gmail.com>
2021-08-01 17:42:19 +0300

    feat: implement dashboard component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import '../styles/index.scss';
 import { APARTMENTS } from '../data/data';
+import Dashboard from './dashboard/Dashboard';
 
 const App = () => <Dashboard apartments={APARTMENTS} />;
 
---
+++ src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+export default class Dashboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSearchValue = this.handleSearchValue.bind(this);
+    this.handleCheckboxValue = this.handleCheckboxValue.bind(this);
+    this.state = {
+      filterText: '',
+      petsAllowed: false,
+    };
+  }
+
+  handleSearchValue(e) {
+    this.setState({
+      filterText: e.target.value,
+    });
+  }
+
+  handleCheckboxValue(e) {
+    this.setState({
+      petsAllowed: e.target.checked,
+    });
+  }
+
+  render() {
+    return (
+      <div className="wrap">
+        <SearchBar
+          petsAllowed={this.state.petsAllowed}
+          filterText={this.state.filterText}
+          onSearchValue={this.handleSearchValue}
+          onCheckboxValue={this.handleCheckboxValue}
+        />
+        <ApartmentTable
+          apartments={this.props.apartments}
+          filterText={this.state.filterText}
+          petsAllowed={this.state.petsAllowed}
+        />
+      </div>
+    );
+  }
+}
