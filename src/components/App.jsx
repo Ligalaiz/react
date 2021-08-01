c7c13567bd22003552cb90307f0e4fc8fb64c654
Ligalaiz <slavakuz88@gmail.com>
2021-08-01 17:30:04 +0300

    feat: add template

---
+++ src/components/App.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import '../styles/index.scss';
+import { APARTMENTS } from '../data/data';
+
+const App = () => <Dashboard apartments={APARTMENTS} />;
+
+export default App;
