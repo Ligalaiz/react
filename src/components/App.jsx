cbc128d8024a72f035d863d8ca55a872a426fd5f
Ligalaiz <slavakuz88@gmail.com>
2021-08-01 18:01:36 +0300

    feat: add animations

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,10 @@
 import '../styles/index.scss';
 import { APARTMENTS } from '../data/data';
 import Dashboard from './dashboard/Dashboard';
+import '../assets/animations/TweenLite.min';
+import '../assets/animations/EasePack.min';
+import '../assets/animations/rAF';
+import '../assets/animations/config';
 
 const App = () => <Dashboard apartments={APARTMENTS} />;
 
