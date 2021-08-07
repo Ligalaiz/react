c4f6906ca6ff6a1db9ca444364b029a6c8afb9d4
Ligalaiz <slavakuz88@gmail.com>
2021-08-07 18:15:22 +0300

    feat: add common file for utils

---
+++ src/utils/index.jsx
@@ -0,0 +1,5 @@
+import isNameValid from './nameValidate';
+import isNumberValid from './numberValidate';
+import isFileValid from './fileValidate';
+
+export { isNameValid, isNumberValid, isFileValid };
