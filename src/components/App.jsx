06daffed478e6fd85942033531a27c86586c769c
Ligalaiz <slavakuz88@gmail.com>
2021-08-10 07:34:19 +0300

    feat: implement SearchBar component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,22 @@
+import { useState } from 'react';
+import SearchBar from './searchBar/SearchBar';
 import '../styles/index.scss';
 
 const App = () => {
+  const [items, setItems] = useState([]);
+  const [searchRequest, setSearchRequest] = useState('');
 
   return (
     <>
-      <div className="control"></div>
+      <div className="control">
+        <div className="form__wrap">
+          <SearchBar
+            searchRequest={searchRequest}
+            setSearchRequest={setSearchRequest}
+            setItems={setItems}
+          />
+        </div>
+      </div>
     </>
   );
 };
---
+++ src/components/searchBar/SearchBar.jsx
@@ -0,0 +1,84 @@
+export default function SearchBar(props) {
+  const { searchRequest, setSearchRequest, setItems } = props;
+
+  const date = new Date();
+  const month = date.getMonth();
+  const day = date.getDate();
+  const currentDate = `${date.getFullYear()}-${
+    month < 10 ? `0${month}` : month
+  }-${day < 10 ? `0${day}` : day}`;
+
+  const link = `https://cors.bridged.cc/${process.env.BASE_PATH}${process.env.SEARCH_PATH}?${process.env.SEARCH_PARAM}${searchRequest}&${process.env.SEARCH_FROM}${currentDate}&${process.env.SEARCH_PAGE_SIZE}10&${process.env.SEARCH_SORT}publishedAt&${process.env.SEARCH_PAGE_NUMBER}1&${process.env.SEARCH_API_KEY}${process.env.API_KEY3}`;
+
+  const data = {
+    method: 'GET',
+    headers: {
+      'X-Requested-With': 'XMLHttpRequest',
+    },
+  };
+
+  const loadSearchRequest = async (url) => {
+    async function delay(response) {
+      return new Promise((res) => {
+        setTimeout(() => res(response), 1500);
+      });
+    }
+
+    try {
+      setItems([]);
+
+      const response = await fetch(url, data).catch((e) =>
+        console.log('Error: ', e.message),
+      );
+
+      let result = response.json();
+      result = await delay(result);
+
+      if (result.status !== 'ok') {
+        throw Error(result.message);
+      }
+
+      setItems(result.articles || []);
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchRequest.length > 2) {
+      loadSearchRequest(link);
+    }
+  };
+
+  const handleSearchChange = (e) => {
+    const { target } = e;
+    const { value } = target;
+    setSearchRequest(value);
+  };
+
+  return (
+    <>
+      <div className="search__wrap">
+        <div className="container">
+          <form className="search__form" onSubmit={handleSubmit}>
+            <div className="search__container">
+              <input
+                className="search__field"
+                placeholder="Start your search"
+                value={searchRequest}
+                onChange={handleSearchChange}
+              />
+              <button
+                type="submit"
+                className="search__btn"
+                disabled={searchRequest < 3}
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
