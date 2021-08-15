5ba5d19a644cd38ba30d3e273caa16c179f857c1
Ligalaiz <slavakuz88@gmail.com>
2021-08-15 09:38:56 +0300

    feat: add link on details page

--- src/components/search/components/searchResult/SearchResult.jsx
+++ src/components/search/components/searchResult/SearchResult.jsx
@@ -1,13 +1,25 @@
+import { Link, useLocation } from 'react-router-dom';
 import Articles from '../articles/Arcicles';
 
 export default function SearchResult(props) {
   const { items } = props;
+  const { search } = useLocation();
 
   if (!items) return false;
+
   return (
     <>
       {items.map((article, index) => (
-        <Articles article={article} key={index} />
+        <Link
+          to={{
+            pathname: `/details/card/${article.id}`,
+            search: `${search}`,
+            state: { article: article, id: article.id, items: items },
+          }}
+          key={index}
+        >
+          <Articles article={article} />
+        </Link>
       ))}
     </>
   );
