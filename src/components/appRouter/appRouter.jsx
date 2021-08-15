04a32fecb0ad675b6fa5bba3ebe3d6f66da885a7
Ligalaiz <slavakuz88@gmail.com>
2021-08-15 08:11:59 +0300

    feat: implement animation for router

--- src/components/appRouter/appRouter.jsx
+++ src/components/appRouter/appRouter.jsx
@@ -1,16 +1,27 @@
 import { Route, Switch, useLocation } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import { routes } from '../../router';
 
 const AppRouter = () => {
   const location = useLocation();
   return (
-    <Switch location={location}>
-      {routes.map(({ path, Component, exact }, idx) => (
-        <Route path={path} exact={exact} key={idx}>
-          <Component />
-        </Route>
-      ))}
-    </Switch>
+    <TransitionGroup>
+      <Switch location={location}>
+        {routes.map(({ path, Component, exact }, idx) => (
+          <Route path={path} exact={exact} key={idx}>
+            {({ match }) => (
+              <CSSTransition
+                timeout={1000}
+                classNames="page"
+                in={match !== null}
+              >
+                <Component />
+              </CSSTransition>
+            )}
+          </Route>
+        ))}
+      </Switch>
+    </TransitionGroup>
   );
 };
 
