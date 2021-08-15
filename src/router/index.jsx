313984d187f277cad645898aa3a1e024512f1614
Ligalaiz <slavakuz88@gmail.com>
2021-08-15 08:29:46 +0300

    feat: add components to router

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,8 +1,12 @@
 import Search from '../components/search/components/Search';
+import Details from '../components/details/details';
 import About from '../components/about/about';
+import Error from '../components/error/error';
 
 export const routes = [
   { path: '/', Component: Search, exact: true },
+  { path: '/details/card/:id', Component: Details, exact: true },
   { path: '/about', Component: About, exact: true },
+  { path: '/error', Component: Error, exact: true },
   { path: '*', Component: Error, exact: false },
 ];
