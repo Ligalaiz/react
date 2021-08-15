6c3d410b8fc4c3f1e2096c534d5117501686a6f0
Ligalaiz <slavakuz88@gmail.com>
2021-08-15 08:40:19 +0300

    feat: implement getUrl utils

--- src/utils/index.jsx
+++ src/utils/index.jsx
@@ -1,5 +1,6 @@
 import { set, get, del } from './storage.utils';
 import getQueryUtils from './getQuery.utils';
 import delayUtils from './delay.utils';
+import getUrlUtils from './getUrl.utils';
 
-export { set, get, del, getQueryUtils, delayUtils };
+export { set, get, del, getQueryUtils, delayUtils, getUrlUtils };
