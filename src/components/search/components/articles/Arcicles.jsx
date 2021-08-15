c4b67db6933a6029814d72f24b33c4e67c1849f3
Ligalaiz <slavakuz88@gmail.com>
2021-08-15 08:16:22 +0300

    feat: add animation for articles

--- src/components/search/components/articles/Arcicles.jsx
+++ src/components/search/components/articles/Arcicles.jsx
@@ -1,29 +1,40 @@
 import defaultImg from '../../assets/img/js.gif';
 
 export default function Articles(props) {
-  const { author, content, description, publishedAt, title, url, urlToImage } =
-    props.article;
+  const { author, description, publishedAt, title, urlToImage } = props.article;
+  const date = new Date(publishedAt);
+  const month = date.getMonth();
+  const day = date.getDay();
+  const currentDate = `${date.getFullYear()}-${
+    month < 10 ? `0${month}` : month
+  }-${day < 10 ? `0${day}` : day}`;
 
   return (
-    <div className="reset-list article__wrap">
+    <div className="article__wrap">
       <div className="article">
         <div className="article__image imgage__wrap">
-          <img
-            width="300"
-            src={urlToImage || defaultImg}
-            alt={title}
-            onError={(e) => {
-              e.target.onerror = null;
-            }}
-          />
+          <div className="rounded-lg">
+            <figure role="presentation" className="bio-image bio-image--white">
+              <div className="bio-image-wrapper">
+                <img
+                  className="bio-image-image"
+                  src={urlToImage || defaultImg}
+                  alt={title}
+                  loading="eager"
+                  decoding="async"
+                  onError={(e) => {
+                    e.target.onerror = null;
+                  }}
+                />
+              </div>
+            </figure>
+          </div>
         </div>
         <div className="article__description">
-          <p>title: {title}</p>
-          <p>content: {content}</p>
-          <p>description: {description}</p>
-          <p>publishedAt: {publishedAt}</p>
-          <p>author: {author}</p>
-          <p>url: {url}</p>
+          <h2 className="article__title">{title}</h2>
+          <p className="short-description">{description}</p>
+          <p>Published at {currentDate}</p>
+          <p>Author: {author}</p>
         </div>
       </div>
     </div>
