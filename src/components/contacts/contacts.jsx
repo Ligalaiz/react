2a091d4115ae640ccd3e7a6470e43a716a658d24
Ligalaiz <slavakuz88@gmail.com>
2021-08-15 09:55:20 +0300

    feat: implement contacts component

---
+++ src/components/contacts/contacts.jsx
@@ -0,0 +1,10 @@
+import rss from '@/assets/img/rss.jpg';
+import './contacts.scss';
+
+const Contacts = () => (
+  <div className="contacts__img">
+    <img src={rss} alt="RSschool" />
+  </div>
+);
+
+export default Contacts;
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,12 +1,14 @@
 import Search from '../components/search/components/Search';
 import Details from '../components/details/details';
 import About from '../components/about/about';
+import Contacts from '../components/contacts/contacts';
 import Error from '../components/error/error';
 
 export const routes = [
   { path: '/', Component: Search, exact: true },
   { path: '/details/card/:id', Component: Details, exact: true },
   { path: '/about', Component: About, exact: true },
+  { path: '/contacts', Component: Contacts, exact: true },
   { path: '/error', Component: Error, exact: true },
   { path: '*', Component: Error, exact: false },
 ];
