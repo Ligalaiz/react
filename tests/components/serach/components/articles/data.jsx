71744a8600de2aaa4c11d5dc4944b1deba4123a4
Ligalaiz <slavakuz88@gmail.com>
2021-08-28 13:55:31 +0300

    feat: add data for articles component

---
+++ tests/components/serach/components/articles/data.jsx
@@ -0,0 +1,8 @@
+const data = {
+  author: 'Reuters',
+  title: "Tesla's self-driving software update 'not great': Elon Musk",
+  description:
+    'The billionaire entrepreneur tweeted that the Full Self-Driving Beta version 9.2 is &quot;actually not great imo (in my opinion), but Autopilot/AI team is rallying to improve as fast as possible.&quot;',
+};
+
+export default data;
