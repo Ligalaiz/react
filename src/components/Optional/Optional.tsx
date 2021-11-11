73be82fa3e14727199d24db7159ccf023fea21df
Ligalaiz <slavakuz88@gmail.com>
2021-11-11 20:25:49 +0300

    feat: implement Optional component

---
+++ src/components/Optional/Optional.tsx
@@ -0,0 +1,31 @@
+import React, { FC, ReactElement } from 'react';
+import { IApartment, ICard } from '@src/interfaces';
+
+export const Optional: FC<ICard> = ({ apartment }) => {
+  const result: ReactElement[] = [];
+  const optionals = [
+    'guests',
+    'bedroom',
+    'bed',
+    'bath',
+    'wifi',
+    'condition',
+    'kitchen',
+    'heating',
+  ];
+
+  const keys = Object.keys(apartment);
+
+  for (const key of keys) {
+    if (optionals.indexOf(key) !== -1 && apartment[key as keyof IApartment]) {
+      result.push(
+        <li className="optional__item" key={key}>
+          {apartment[key as keyof IApartment]} {key}
+          {key === optionals[optionals.length - 1] ? '' : ' · '}
+        </li>,
+      );
+    }
+  }
+
+  return <>{result}</>;
+};
---
+++ src/components/Optional/index.ts
@@ -0,0 +1,2 @@
+export { Optional } from './Optional';
+;
