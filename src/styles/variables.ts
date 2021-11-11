615b90df5da0683c06ff1cfb26057a9874ace61a
Ligalaiz <slavakuz88@gmail.com>
2021-11-11 20:30:36 +0300

    feat: add global styles

---
+++ src/styles/variables.ts
@@ -0,0 +1,16 @@
+interface IColor {
+  [key: string]: string;
+}
+
+export const color: IColor = {
+  'brand-color': '#FF385C',
+  'brand-color-light': '#ff00aa',
+  'white-color': '#ffffff',
+  'grey-color': '#adb5bd',
+  'grey-color--light': '#DDDDDD',
+  'grey-color--dark': '#717171',
+  'black-color': '#222222',
+  'black-color--light': '#00000080',
+  'black-color--lightX0': '#00000018',
+  'black-color--lightX1': '#00000005',
+};
