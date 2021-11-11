6b8c0d549689a1d4d1629110e3b8ac7d929a4a5f
Ligalaiz <slavakuz88@gmail.com>
2021-11-11 20:21:01 +0300

    feat: add styles for SearchBar component

---
+++ src/components/SearchBar/SearchBarStyles.ts
@@ -0,0 +1,21 @@
+import { css } from '@emotion/react';
+import { color } from '@src/styles';
+
+export const searchWrap = css`
+  grid-area: search;
+`;
+
+export const searchBtnWrap = css`
+  position: absolute;
+  right: 5px;
+  top: 10%;
+`;
+
+export const searchContainer = css`
+  position: relative;
+
+  display: inline-block;
+  margin-bottom: 10px;
+
+  overflow: visible;
+`;
