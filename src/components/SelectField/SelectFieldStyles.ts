29ed7daab3f23cde2a42c2e31bc882017f637c0c
Ligalaiz <slavakuz88@gmail.com>
2021-11-16 17:51:06 +0300

    feat: add styles for SelectField component

---
+++ src/components/SelectField/SelectFieldStyles.ts
@@ -0,0 +1,15 @@
+import { css } from '@emotion/react';
+import { color } from '@src/styles';
+
+export const experianceSelect = css`
+  width: 150px;
+
+  color: var(--primary-color);
+
+  outline: none;
+  background-color: transparent;
+  cursor: pointer;
+`;
+export const experianceItem = css`
+  color: ${color['black-color']};
+`;
