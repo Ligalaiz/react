969599ebce4058144656ef7643efc0a3d609be22
Ligalaiz <slavakuz88@gmail.com>
2021-11-16 16:52:37 +0300

    feat: add styles for AgreementField component

--- src/components/AgreementField/AgreementField.tsx
+++ src/components/AgreementField/AgreementField.tsx
@@ -1,33 +1,39 @@
 /** @jsxImportSource @emotion/react */
 import { IAgreementField } from '@src/interfaces';
 import React, { FC } from 'react';
-import { agreementStyle } from './AgreementFieldStyle';
+import {
+  agreementLink,
+  agreementCheckbox,
+  agreementLabel,
+  agreementWrap
+} from './AgreementFieldStyle';
 
 export const AgreementField: FC<IAgreementField> = ({
   agreement,
   handleClick,
 }) => {
   return (
-    <div className="agreement" css={agreementStyle}>
-      <label className="agreement__checkbox">
-        <input
-          name="agreement"
-          type="checkbox"
-          defaultChecked={agreement}
-          onClick={handleClick}
-          data-testid="agreementField"
-        />
-        <span>
-          I accept the{' '}
-          <a
-            className="agreement__link"
-            target="_blank="
-            href="http://www.it-lex.ru/usloviya_ispolzovaniya_servisa/obrazec-politiki-konfidencialnosti-2017/"
-          >
-            agreement *
-          </a>
-        </span>
+    <div className="agreement__wrap" css={agreementWrap}>
+      <input
+        name="agreement"
+        type="checkbox"
+        css={agreementCheckbox}
+        id='agreement'
+        defaultChecked={agreement}
+        onClick={handleClick}
+        data-testid="agreementField"
+      />
+      <label htmlFor="agreement" className="agreement__checkbox" css={agreementLabel}>
+        I accept the{' '}
       </label>
+      <a
+        className="agreement__link"
+        css={agreementLink}
+        target="_blank="
+        href="http://www.it-lex.ru/usloviya_ispolzovaniya_servisa/obrazec-politiki-konfidencialnosti-2017/"
+      >
+        agreement *
+      </a>
     </div>
   );
 };
---
+++ src/components/AgreementField/AgreementFieldStyle.ts
@@ -0,0 +1,80 @@
+import { css } from '@emotion/react';
+import { color } from '@src/styles';
+
+export const agreementLink = css`
+  text-decoration: none;
+  color: var(--secondary-color);
+`;
+
+export const agreementCheckbox = css`
+  position: absolute;
+  z-index: -1;
+  opacity: 0;
+  &:checked + label {
+    &::before {
+      border-color: ${color['brand-color']};
+      background-color: ${color['brand-color']};
+      background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8'%3e%3cpath fill='%23fff' d='M6.564.75l-3.59 3.612-1.538-1.55L0 4.26 2.974 7.25 8 2.193z'/%3e%3c/svg%3e");
+    }
+  }
+  &:not(:disabled) {
+    &:not(:checked) + label {
+      &:hover {
+        &::before {
+          border-color: ${color['grey-color--light']};
+        }
+      }
+    }
+    &:active + label {
+      &::before {
+        border-color: ${color['grey-color--light']};
+        background-color: ${color['black-color--light']};
+      }
+    }
+  }
+  &:focus + label {
+    &::before {
+      box-shadow: 0 0 0 0.2rem ${color['brand-color-light']};
+    }
+  }
+  &:focus {
+    &:not(:checked) + label {
+      &::before {
+        border-color: ${color['black-color--light']};
+      }
+    }
+  }
+  &:disabled + label {
+    &::before {
+      background-color: ${color['black-color--light']};
+    }
+  }
+`;
+
+export const agreementLabel = css`
+  display: inline-flex;
+  align-items: center;
+  user-select: none;
+  margin-right: 10px;
+
+  &::before {
+    content: '';
+    display: inline-block;
+    width: 1em;
+    height: 1em;
+    flex-shrink: 0;
+    flex-grow: 0;
+    border: 1px solid ${color['grey-color']};
+    border-radius: 0.25em;
+    margin-right: 0.5em;
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: 50% 50%;
+    cursor: pointer;
+  }
+`;
+
+export const agreementWrap = css`
+  display: flex;
+  max-width: 204px;
+`;
