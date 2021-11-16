db694447f4e2f41713cfa4a9ff447e64f6786de0
Ligalaiz <slavakuz88@gmail.com>
2021-11-16 17:58:00 +0300

    feat: add styles for BirthdayField component

---
+++ src/components/BirthdayField/BirthdayFieldStyle.ts
@@ -0,0 +1,11 @@
+import { css } from '@emotion/react';
+
+export const birthdayField = css`
+  border: none;
+
+  color: var(--primary-color);
+
+  outline: none;
+  background: transparent;
+  cursor: pointer;
+`;
