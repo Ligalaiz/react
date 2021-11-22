6a4f9069b726440b51e9a05c73bee9c65a159309
Ligalaiz <slavakuz88@gmail.com>
2021-11-22 18:24:05 +0300

    feat: implement api request method

---
+++ src/utils/http.utils.ts
@@ -0,0 +1,21 @@
+import { IResponse, IData } from '@src/interfaces';
+
+export async function http(request: string, data: IData): Promise<IResponse> {
+  async function delay<T>(response: T): Promise<T> {
+    return new Promise((res) => {
+      setTimeout(() => res(response), 1500);
+    });
+  }
+  let response = await fetch(request, data);
+  response = await delay(response);
+  let result;
+  try {
+    result = await response.json();
+  } catch (e: any) {
+    throw Error(e.message);
+  }
+  if (result.status !== 'ok') {
+    throw Error(result.message);
+  }
+  return result;
+}
