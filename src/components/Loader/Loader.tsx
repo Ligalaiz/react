9477361e9ae0ac77ed183d5d5f3d98bf3f99a28b
Ligalaiz <slavakuz88@gmail.com>
2021-11-22 18:45:16 +0300

    feat: implement Loader component

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,19 @@
+/** @jsxImportSource @emotion/react */
+import React, { FC } from 'react';
+import { loader } from './LoaderStyles'
+
+export const Loader: FC = () => {
+  return (
+    <div className="loader" css={loader}>
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  );
+}
