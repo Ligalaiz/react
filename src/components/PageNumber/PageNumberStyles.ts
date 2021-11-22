75f8b5d984667dfdbb1ba510322013447679ab93
Ligalaiz <slavakuz88@gmail.com>
2021-11-22 18:37:25 +0300

    feat: add styles for PageNumber component

---
+++ src/components/PageNumber/PageNumberStyles.ts
@@ -0,0 +1,12 @@
+import { css } from '@emotion/react';
+
+export const pageNumberStyle = css`
+  padding: 10px;
+  border: none;
+  border-right: none;
+  max-width: 120px;
+
+  text-align: center;
+
+  cursor: pointer;
+`;
